33db4b27724cab7baab78946033358bb0fd7e6ab
Dipokal HHJ <hhj@devent.kr>
2025-07-22 19:37:45 +0900

    feat: update ui

--- src/components/three/Text.tsx
+++ src/components/three/Text.tsx
@@ -1,45 +1,47 @@
 import { Center, MeshTransmissionMaterial, Text3D } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import type { Mesh } from "three";
 
-export default function My3DText() {
+export default function GlassText({ text }: { text: string }) {
   const fontUrl = "/font/Pretendard_Bold.json";
 
+  const textRef = useRef<Mesh>(null);
+
+  useFrame((state) => {
+    if (textRef.current) {
+      textRef.current.rotation.y = state.clock.getElapsedTime() * 0.8;
+    }
+  });
+
   return (
-    <Center>
-      <Text3D
-        size={1}
-        font={fontUrl}
-        height={0.5}
-        curveSegments={12}
-        bevelEnabled
-        bevelThickness={0.05}
-        bevelSize={0.05}
-      >
-        3D TEXT
-        {/* <meshPhysicalMaterial
-          transmission={1.0}
-          roughness={0.0}
-          metalness={0.0}
-          ior={1.5}
-          thickness={0.5}
-          dispersion={0.5}
-          specularIntensity={1.0}
-          color="white"
-        /> */}
-        {/* <meshMatcapMaterial map={textureMat} /> */}
-        <MeshTransmissionMaterial
-          background={undefined}
-          backside={true}
-          anisotropicBlur={0.1}
-          transmission={0.999}
-          roughness={0.04}
-          thickness={0.03}
-          ior={10.5}
-          chromaticAberration={0.1}
-          anisotropy={0.1}
-          emissive="#000000"
-          emissiveIntensity={1}
-        />
-      </Text3D>
-    </Center>
+    <group ref={textRef} position={[0, 0, 0]}>
+      <Center>
+        <Text3D
+          size={1}
+          font={fontUrl}
+          height={0.5}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.05}
+          bevelSize={0.05}
+        >
+          {text}
+          <MeshTransmissionMaterial
+            background={undefined}
+            backside={true}
+            anisotropicBlur={0.1}
+            transmission={0.999}
+            roughness={0.04}
+            thickness={0.03}
+            ior={10.5}
+            chromaticAberration={0.1}
+            anisotropy={0.1}
+            emissive="#000000"
+            emissiveIntensity={1}
+          />
+        </Text3D>
+      </Center>
+    </group>
   );
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,83 +1,167 @@
+import React from "react";
+import { Home, Package, Menu, Download } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { Canvas, useLoader } from "@react-three/fiber";
 import { Environment, Lightformer, OrbitControls } from "@react-three/drei";
-import My3DText from "@/components/three/Text";
 import { EquirectangularReflectionMapping, TextureLoader } from "three";
 import {
   EffectComposer,
   Bloom,
   ChromaticAberration,
 } from "@react-three/postprocessing";
+import GlassText from "@/components/three/Text";
 
-function GlassWithLocalReflection() {
+const Sidebar = () => {
+  const menuItems = [
+    { name: "Dashboard", icon: Home },
+    { name: "Products", icon: Package },
+  ];
+
+  const sidebarContent = (
+    <div className="flex flex-col h-full text-white mt-4">
+      <nav className="flex-1 px-2">
+        {menuItems.map((item) => (
+          <a
+            key={item.name}
+            href="#"
+            className="flex items-center px-4 py-2 mt-2 text-sm font-medium rounded-md hover:bg-gray-700"
+          >
+            <item.icon className="w-5 h-5 mr-3" />
+            {item.name}
+          </a>
+        ))}
+      </nav>
+    </div>
+  );
+
+  return (
+    <>
+      <div className="hidden lg:block w-64 bg-black border-r border-gray-800 shrink-0">
+        {sidebarContent}
+      </div>
+      <div className="lg:hidden">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="fixed top-4 left-4 z-50 text-gray-800 dark:text-white"
+            >
+              <Menu className="w-6 h-6" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-64 bg-gray-800 p-0 border-r-0">
+            {sidebarContent}
+          </SheetContent>
+        </Sheet>
+      </div>
+    </>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <header className="flex items-center justify-end h-16 px-6 bg-black border-b border-gray-800 dark:bg-gray-800 dark:border-gray-700 shrink-0">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="default">
+            <Download />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-56">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Log out</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </header>
+  );
+};
+
+const ThreeScene = () => {
   const texture = useLoader(TextureLoader, "/image/texture.png");
-
   texture.mapping = EquirectangularReflectionMapping;
 
-  return <Environment map={texture} />;
-}
+  return (
+    <Canvas
+      camera={{ position: [0, 0, 7] }}
+      className="rounded-lg border-2 border-gray-800"
+      style={{ width: "512px", height: "512px" }}
+    >
+      <color attach="background" args={["#000000"]} />
+      <ambientLight intensity={0.5} color={"#ffffff"} />
+      <directionalLight
+        position={[0, 100, 60]}
+        intensity={30}
+        color={"#ffffff"}
+      />
+      <Environment>
+        <Lightformer
+          form="rect"
+          intensity={4}
+          position={[-5, 5, -5]}
+          scale={4}
+        />
+        <Lightformer
+          form="rect"
+          intensity={4}
+          position={[5, 10, 5]}
+          scale={10}
+          map={texture}
+        />
+        <Lightformer
+          form="rect"
+          intensity={4}
+          position={[5, -5, 5]}
+          scale={10}
+          map={texture}
+        />
+        <Lightformer
+          form="rect"
+          intensity={4}
+          position={[-5, 0, 15]}
+          scale={30}
+          map={texture}
+        />
+      </Environment>
+      <GlassText text="3D TEXT" />
+      <EffectComposer>
+        <Bloom
+          intensity={1.5}
+          luminanceThreshold={0.5}
+          luminanceSmoothing={0.025}
+          mipmapBlur
+        />
+        <ChromaticAberration offset={[0.001, 0.001]} />
+      </EffectComposer>
+      <OrbitControls />
+    </Canvas>
+  );
+};
 
 export default function HomePage() {
-  const texture = useLoader(TextureLoader, "/image/texture.png");
-
-  texture.mapping = EquirectangularReflectionMapping;
   return (
-    <main className="h-full w-full h-full">
-      <Canvas camera={{ position: [0, 0, 5] }}>
-        <color attach="background" args={["#000000"]} />
-        <ambientLight intensity={0.5} color={"#ffffff"} />
-        <pointLight position={[10, 10, 10]} intensity={30} color={"#ffffff"} />
-        <directionalLight
-          position={[0, 100, 60]}
-          intensity={30}
-          color={"#ffffff"}
-        />
-
-        <Environment>
-          <Lightformer
-            form="rect"
-            intensity={4}
-            position={[-5, 5, -5]}
-            scale={4}
-          />
-          <Lightformer
-            form="rect"
-            intensity={4}
-            position={[5, 10, 5]}
-            scale={10}
-            map={texture}
-          />
-          <Lightformer
-            form="rect"
-            intensity={4}
-            position={[5, -5, 5]}
-            scale={10}
-            map={texture}
-          />
-          <Lightformer
-            form="rect"
-            intensity={4}
-            position={[-5, 0, 15]}
-            scale={30}
-            map={texture}
-          />
-        </Environment>
-
-        {/* <GlassWithLocalReflection /> */}
-
-        <My3DText />
-
-        <EffectComposer>
-          <Bloom
-            intensity={1.5}
-            luminanceThreshold={0.5}
-            luminanceSmoothing={0.025}
-            mipmapBlur
-          />
-          <ChromaticAberration offset={[0.001, 0.001]} />
-        </EffectComposer>
-
-        <OrbitControls />
-      </Canvas>
-    </main>
+    <div className="flex w-screen h-screen bg-black dark:bg-gray-950">
+      <Sidebar />
+      <div className="flex flex-col flex-1">
+        <Navbar />
+        <main className="flex-1 p-6">
+          <div className="flex w-full h-full justify-center p-4 bg-black rounded-lg shadow-md dark:bg-gray-900">
+            <ThreeScene />
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
