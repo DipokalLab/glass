a6f740f3b4b4de8d8a7da4854873101fd25abf8f
Huh Hyeongjun <hhj@devent.kr>
2025-07-30 16:49:22 +0900

    feat: update navbar

--- src/features/navbar/index.tsx
+++ src/features/navbar/index.tsx
@@ -4,13 +4,23 @@ import { MountainIcon } from "lucide-react";
 import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router";
 
-export const Navbar = () => {
+export const Navbar = ({ isShowLogo = false }: { isShowLogo?: boolean }) => {
   return (
     <header className="flex fixed w-full items-center justify-end h-16 px-6 gap-2 bg-white border-b border-gray-200 dark:bg-black dark:border-gray-800 shrink-0">
-      <Link to="/" className="flex items-center justify-center hidden lg:block">
-        <MountainIcon className="h-6 w-6" />
-        <span className="sr-only">MyBrand</span>
-      </Link>
+      {isShowLogo ? (
+        <Link to="/" className="flex items-center justify-center">
+          <MountainIcon className="h-6 w-6" />
+          <span className="sr-only">MyBrand</span>
+        </Link>
+      ) : (
+        <Link
+          to="/"
+          className="flex items-center justify-center hidden lg:block"
+        >
+          <MountainIcon className="h-6 w-6" />
+          <span className="sr-only">MyBrand</span>
+        </Link>
+      )}
       <nav className="ml-auto flex gap-2 sm:gap-3">
         <ModeToggle />
 
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,7 +5,7 @@ import { Navbar } from "@/features/navbar";
 export default function LandingPage() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
-      <Navbar />
+      <Navbar isShowLogo={true} />
       <main className="flex-1  pt-15">
         <section className="w-full h-full flex items-center justify-center py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="container px-4 md:px-6">
